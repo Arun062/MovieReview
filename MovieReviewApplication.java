0a5b7dab6a97628aed0c1ba7c63f72e8560f53e7
Arun062 <113620300+Arun062@users.noreply.github.com>
2024-04-15 18:21:02 +0530

    Add files via upload

---
+++ MovieReviewApplication.java
@@ -0,0 +1,14 @@
+package deve.arun.movie.movieReview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieReviewApplication.class, args);
+
+	}
+
+}
